e5617f344d94fe3e85ccaf387252f8be084ce984
felipepineda40academy <felipe.pineda@academiacuatropuntocero.com>
2022-05-02 20:15:32 -0600

    trabajo en clase 2-5

---
+++ index.js
@@ -0,0 +1,14 @@
+//1. Hacer un if con operadores logicos que revise la siguiente condicion:
+// Si mi edad es menor que 20 años o mayor a 60, *en mi cumpleaños compran helados de chocochips *esto es el console.log
+
+//1.5 if con operadores logicos que revise la siguiente condicion
+// si mi perro esta en la casa y mi gato tambien, *entonces estoy feliz! var perro = true; 
+
+var perro = true;
+var gato = true;
+
+//2. Hacer un counter que imprima un numero del 1 al 100 pero por cada iteracion no se le suma 1, sino 3 unidades 
+//ejemplo:
+//1
+//4
+//7
